efdc6e9a007d35c8475d682035a7347e14079ccb
ruobing guan <hello@robinguan.com>
2020-03-16 03:58:47 -0700

    commit

--- theme/src/components/TwoColumnsGrid.js
+++ theme/src/components/TwoColumnsGrid.js
@@ -5,12 +5,12 @@ const TwoColumnsGrid = ({ left, right }) => {
   return (
     <Grid container spacing={3}>
       <Grid item sm={6} xs={12}>
-        <Box display="flex" justifyContent="flex-start" mr={0.5}>
+        <Box display="flex" justifyContent="flex-start" m={1}>
           {left}
         </Box>
       </Grid>
       <Grid item sm={6} xs={12}>
-        <Box display="flex" justifyContent="flex-start" ml={0.5}>
+        <Box display="flex" justifyContent="flex-start" m={1}>
           {right}
         </Box>
       </Grid>
